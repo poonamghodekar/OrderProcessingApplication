29a86380af9de82fe26c72726ca301590bb3dd31
Poonam Ghodekar <37392965+poonamghodekar@users.noreply.github.com>
2020-09-13 20:26:14 +0530

    Added Business rules using strategy pattern

---
+++ OrderProcessing/OrderProcessing.Domain/BusinessRules/OrderProcessingBaseStrategy.cs
@@ -0,0 +1,17 @@
+﻿using OrderProcessing.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderProcessing.Domain.BusinessRules
+{
+    /// <summary>
+    /// Strategy pattern
+    /// </summary>
+    public interface IOrderProcessingBaseStrategy
+    {
+        void ProcessOrder(IOrder order);
+    }
+}
---
+++ OrderProcessing/OrderProcessing.Domain/BusinessRules/ProductOrderProcessingStrategy.cs
@@ -0,0 +1,17 @@
+﻿using OrderProcessing.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderProcessing.Domain.BusinessRules
+{
+    public class ProductOrderProcessingStrategy : IOrderProcessingBaseStrategy
+    {
+        public void ProcessOrder(IOrder order)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
