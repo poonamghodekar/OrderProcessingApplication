2c44ee7a7cfdf51aeda0281764dde443ae91b6e4
Poonam Ghodekar <37392965+poonamghodekar@users.noreply.github.com>
2020-09-13 20:40:10 +0530

    Added order processing service
    Added order processing service and update Program.cs login for order processing menu

---
+++ OrderProcessing/OrderProcessing.Domain/Services/OrderProcessingService.cs
@@ -0,0 +1,24 @@
+﻿using OrderProcessing.Domain.BusinessRules;
+using OrderProcessing.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderProcessing.Domain.Services
+{
+    public class OrderProcessingService
+    {
+        private IOrderProcessingBaseStrategy _orderProcessingBaseStrategy;
+        public void SetOrderProcessingStrategy(IOrderProcessingBaseStrategy orderProcessingBaseStrategy)
+        {
+            _orderProcessingBaseStrategy = orderProcessingBaseStrategy;
+        }
+
+        public void ProcessOrder(IOrder order)
+        {
+            _orderProcessingBaseStrategy.ProcessOrder(order);
+        }
+    }
+}
--- OrderProcessing/OrderProcessing/Program.cs
+++ OrderProcessing/OrderProcessing/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using OrderProcessing.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,7 +11,54 @@ namespace OrderProcessing
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello!!");
+            int option = 0;
+            Console.WriteLine("\n//////////////////////////////////////// \n");
+            Console.WriteLine("Place your order for \n");
+            Console.WriteLine("******************************** \n");
+            Console.WriteLine("1: Product \n" +
+                              "2: Membership\n" +
+                              "3: Video \n" +
+                              "4: Process Orders\n" +
+                              "5: Exit\n");
+
+            Console.WriteLine("********************************");
+            option = Convert.ToInt32(Console.ReadLine());
+
+            switch (option)
+            {
+                case 1:
+                    Console.WriteLine("Order has been placed for " + OrderType.PRODUCT + "\n");
+                    Console.WriteLine("Please choose which type of order you want to place :\n");
+                    Console.WriteLine("1: Physical \n" +
+                              "2: Book\n");
+                    Console.WriteLine("Enter your choice :");
+                    break;
+                case 2:
+                    Console.WriteLine("Order has been placed for " + OrderType.MEMBERSHIP + "\n");
+                    Console.WriteLine("Please choose which type of order you want to place :\n");
+                    Console.WriteLine("1: Activate \n" +
+                              "2: Upgrade\n");
+                    Console.WriteLine("Enter your choice :");
+                    break;
+                case 3:
+                    Console.WriteLine("Order has been placed for " + OrderType.VIDEO + "\n");
+                    Console.WriteLine("Please choose which type of order you want to place :\n");
+                    Console.WriteLine("1: Learning \n" +
+                              "2: Entertainment\n");
+                    Console.WriteLine("Enter your choice :");
+                    break;
+                case 4:
+                    ProcessAllOrders();
+                    break;
+                case 5:
+                    Console.WriteLine("You have pressed Exit");
+                    break;
+                default:
+                    break;
+            }
         }
+
+        private static void ProcessAllOrders()
+        { }
     }
 }
