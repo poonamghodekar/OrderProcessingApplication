bb7494d41c78e20e05c46ec9675cd4a3cbc8d186
Poonam Ghodekar <37392965+poonamghodekar@users.noreply.github.com>
2020-09-13 20:42:49 +0530

    Added OrderExecutionService

---
+++ OrderProcessing/OrderProcessing.Domain/Services/OrderExecutionService.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderProcessing.Domain.Services
+{
+    public interface IOrderExecutionService
+    {
+        void CreatePackingSlip();
+        void CreateDuplicateSlip();
+        void ActivateMemberShip();
+        void UpgradeMemberShip();
+        void SendEmail(string message);
+        void SendFreeVideo();
+        void GenerateAgentCommission();
+    }
+    class OrderExecutionService : IOrderExecutionService
+    {
+        public void ActivateMemberShip()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CreateDuplicateSlip()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CreatePackingSlip()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GenerateAgentCommission()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SendEmail(string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SendFreeVideo()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpgradeMemberShip()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
