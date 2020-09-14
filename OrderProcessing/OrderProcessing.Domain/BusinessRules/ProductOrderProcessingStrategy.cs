10c3cc657c30d9ffeed459570b809f9be8fbb440
Poonam Ghodekar <37392965+poonamghodekar@users.noreply.github.com>
2020-09-14 12:54:34 +0530

    Code cleanup

--- OrderProcessing/OrderProcessing.Domain/BusinessRules/ProductOrderProcessingStrategy.cs
+++ OrderProcessing/OrderProcessing.Domain/BusinessRules/ProductOrderProcessingStrategy.cs
@@ -1,11 +1,6 @@
 ﻿using OrderProcessing.Domain.Services;
 using OrderProcessing.Models;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace OrderProcessing.Domain.BusinessRules
 {
--- OrderProcessing/OrderProcessing/Program.cs
+++ OrderProcessing/OrderProcessing/Program.cs
@@ -3,9 +3,6 @@ using OrderProcessing.Domain.Services;
 using OrderProcessing.Models;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace OrderProcessing
 {
