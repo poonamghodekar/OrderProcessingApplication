12879b9f7f19b7cc62475d2e874f1bf3afb6a842
Poonam Ghodekar <37392965+poonamghodekar@users.noreply.github.com>
2020-09-14 12:52:48 +0530

    Added remaining unit tests

---
+++ OrderProcessing/OrderProcessing.UnitTests/MembershipProcessingStrategyTests.cs
@@ -0,0 +1,62 @@
+﻿using FizzWare.NBuilder;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NSubstitute;
+using NSubstituteAutoMocker;
+using OrderProcessing.Domain.BusinessRules;
+using OrderProcessing.Domain.Services;
+using OrderProcessing.Models;
+using System;
+
+namespace OrderProcessing.UnitTests
+{
+    [TestClass]
+    public class MembershipProcessingStrategyTests
+    {
+        private readonly NSubstituteAutoMocker<MembershipProcessingStrategy> _autoMocker =
+           new NSubstituteAutoMocker<MembershipProcessingStrategy>();
+        private MembershipProcessingStrategy _sut;
+        private IOrder _order;
+        private IOrderExecutionService _orderExecutionService;
+        private const string EXCEPTION_MESSAGE = "Order is null";
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _sut = _autoMocker.ClassUnderTest;
+            _order = Builder<Membership>.CreateNew()
+                            .With(o => o.MembershipType = MembershipType.ACTIVATE)
+                            .With(o => o.MembershipDetails = Builder<MembershipDetails>.CreateNew().Build())
+                            .With(o => o.OrderId = Guid.NewGuid())
+                            .Build();
+            _orderExecutionService = _autoMocker.Get<IOrderExecutionService>();
+        }
+
+        [TestMethod]
+        public void OrderPlaced_BookType_OrderProcessedSuccessfully()
+        {
+            //arrange
+            _sut.ProcessOrder(_order);
+
+            //assert
+            _orderExecutionService.Received(1);
+        }
+
+        [TestMethod]
+        public void OrderPlaced_OrderIsNull_ThrowsException()
+        {
+            //arrange
+            try
+            {
+                _order = null;
+                _sut.ProcessOrder(_order);
+                Assert.Fail("No exception thrown");
+            }
+            catch (Exception ex)
+            {
+                //assert
+                _orderExecutionService.Received(0);
+                Assert.IsTrue(ex.Message is EXCEPTION_MESSAGE);
+            }
+        }
+    }
+}
---
+++ OrderProcessing/OrderProcessing.UnitTests/VideoRequestProcessingStrategyTests.cs
@@ -0,0 +1,64 @@
+﻿using FizzWare.NBuilder;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NSubstitute;
+using NSubstituteAutoMocker;
+using OrderProcessing.Domain.BusinessRules;
+using OrderProcessing.Domain.Services;
+using OrderProcessing.Models;
+using System;
+
+namespace OrderProcessing.UnitTests
+{
+    [TestClass]
+    public class VideoRequestProcessingStrategyTests
+    {
+        private readonly NSubstituteAutoMocker<VideoRequestProcessingStrategy> _autoMocker =
+           new NSubstituteAutoMocker<VideoRequestProcessingStrategy>();
+        private VideoRequestProcessingStrategy _sut;
+        private IOrder _order;
+        private IOrderExecutionService _orderExecutionService;
+        private const string EXCEPTION_MESSAGE = "Order is null";
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _sut = _autoMocker.ClassUnderTest;
+            _order = Builder<Video>.CreateNew()
+                                .With(o => o.VideoType = VideoType.LEARNING)
+                                .With(o => o.VideoDetails = Builder<VideoDetails>.CreateNew().Build())
+                                .With(o => o.VideoDetails.VideoName = "Learning to Ski")
+                                .With(o => o.OrderId = Guid.NewGuid())
+                                .Build();
+            _orderExecutionService = _autoMocker.Get<IOrderExecutionService>();
+        }
+        [TestMethod]
+        public void OrderPlaced_BookType_OrderProcessedSuccessfully()
+        {
+            //arrange
+            _sut.ProcessOrder(_order);
+
+            //assert
+            _orderExecutionService.Received(1);
+        }
+
+        [TestMethod]
+        public void OrderPlaced_OrderIsNull_ThrowsException()
+        {
+            //arrange
+            try
+            {
+                _order = null;
+                _sut.ProcessOrder(_order);
+                Assert.Fail("No exception thrown");
+            }
+            catch (Exception ex)
+            {
+                //assert
+                _orderExecutionService.Received(0);
+                Assert.IsTrue(ex.Message is EXCEPTION_MESSAGE);
+            }
+        }
+    }
+
+    
+}
